0ab4b5c75f635fab4864a9e3ec97fa63f7504925
niharikasrivatsa <niharika.kasagar@gmail.com>
2022-11-25 01:53:05 -0500

    add onboarding button as component

---
+++ src/components/OnboardingButton.js
@@ -0,0 +1,32 @@
+import {Text, TouchableOpacity} from 'react-native';
+import React from 'react';
+
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
+export default function OnboardingButton({label, onPress}) {
+    return(
+        <TouchableOpacity
+          onPress={onPress}
+          style={{
+            backgroundColor: '#8265DB',
+            padding: 15,
+            width: '80%',
+            borderRadius: 8,
+            marginTop: 50,
+            flexDirection: 'row',
+            justifyContent: 'center',
+          }}>
+        <Text
+          style={{
+            color: 'white',
+            fontSize: 17,
+            textAlign: 'center',
+            fontFamily: 'Nunito_700Bold',
+            marginRight: 8,
+          }}>
+          {label}  
+        </Text>
+        <MaterialIcons name="arrow-right-alt" size={22} color="#fff" />
+      </TouchableOpacity>
+    );
+}
--- src/screens/OnboardingScreen.js
+++ src/screens/OnboardingScreen.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { SafeAreaView, View, Text, TouchableOpacity, Image } from 'react-native';
 
-import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import OnboardingButton from '../components/OnboardingButton';
 
 const OnboardingScreen = () => {
   return (
@@ -20,29 +20,7 @@ const OnboardingScreen = () => {
           }}
           source={require("../assets/images/ProjectifyIcon.png")} />
       </View>
-      <TouchableOpacity
-        style={{
-          backgroundColor: '#8265DB',
-          padding: 15,
-          width: '80%',
-          borderRadius: 8,
-          marginTop: 50,
-          flexDirection: 'row',
-          justifyContent: 'center',
-        }}
-        onPress={() => navigation.navigate('Login')}>
-        <Text
-          style={{
-            color: 'white',
-            fontSize: 17,
-            textAlign: 'center',
-            fontFamily: 'Nunito_700Bold',
-            marginRight: 8,
-          }}>
-          Let's Begin  
-        </Text>
-        <MaterialIcons name="arrow-right-alt" size={22} color="#fff" />
-      </TouchableOpacity>
+      <OnboardingButton label={"Let's Begin"} onPress={()=>{}} />
     </SafeAreaView>
   );
 };
