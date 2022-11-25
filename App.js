65bb43a8c2a9a537c893acb9dabc667a0c0479d1
niharikasrivatsa <niharika.kasagar@gmail.com>
2022-11-25 01:37:54 -0500

    added font and button

--- App.js
+++ App.js
@@ -1,23 +1,20 @@
 import * as React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
 import AuthStack from './src/navigations/AuthStack';
+import { useFonts, Nunito_400Regular, Nunito_700Bold } from '@expo-google-fonts/nunito';
 import {NavigationContainer} from '@react-navigation/native';
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Nunito_400Regular, Nunito_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
     <NavigationContainer>
       <AuthStack />
     </NavigationContainer>
 );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- src/screens/OnboardingScreen.js
+++ src/screens/OnboardingScreen.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { SafeAreaView, View, Text, TouchableOpacity, Image } from 'react-native';
 
-// import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 const OnboardingScreen = () => {
   return (
@@ -12,7 +12,7 @@ const OnboardingScreen = () => {
         alignItems: 'center',
         backgroundColor: '#fff',
       }}>
-      <View style={{flex: '1', justifyContent: 'center', alignItems: 'center'}}>
+      <View style={{justifyContent: 'center', alignItems: 'center'}}>
         <Image 
           style={{ 
             width: 300, 
@@ -20,6 +20,29 @@ const OnboardingScreen = () => {
           }}
           source={require("../assets/images/ProjectifyIcon.png")} />
       </View>
+      <TouchableOpacity
+        style={{
+          backgroundColor: '#8265DB',
+          padding: 15,
+          width: '80%',
+          borderRadius: 8,
+          marginTop: 50,
+          flexDirection: 'row',
+          justifyContent: 'center',
+        }}
+        onPress={() => navigation.navigate('Login')}>
+        <Text
+          style={{
+            color: 'white',
+            fontSize: 17,
+            textAlign: 'center',
+            fontFamily: 'Nunito_700Bold',
+            marginRight: 8,
+          }}>
+          Let's Begin  
+        </Text>
+        <MaterialIcons name="arrow-right-alt" size={22} color="#fff" />
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
