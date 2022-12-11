ccc6b365d3441903c2ec272fcfdb42a577f62fbf
niharikasrivatsa <niharika.kasagar@gmail.com>
2022-12-10 19:02:03 -0500

    refactor few onbaording screens for bacekend

--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -4,6 +4,8 @@ import {
   View,
   Text,
   Image,
+  KeyboardAvoidingView,
+  StyleSheet
 } from 'react-native';
 
 import InputField from '../components/InputField';
@@ -11,101 +13,111 @@ import OnboardingButton from '../components/OnboardingButton';
 import OnboardingBackButton from '../components/onboardingBackButton';
 
 const RegisterScreen = ({navigation}) => {
+  const [username, setUsername] = useState('');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleRegister = () => {
+    console.log("Regsiter");
+    navigation.navigate('Verification')
+  };
+
+  const onBackButtonPressed = () => {
+    navigation.navigate("Login");
+  };
+
   return (
-    
-    <SafeAreaView style={{
-      flex: 1, 
-      justifyContent: 'center',
-      backgroundColor: '#F4F6F9',
-      }}>
-        <View style={{alignItems: 'flex-start'}} >
-        <OnboardingBackButton onPress={()=>navigation.navigate('Login')}/>
-        </View>
+    <KeyboardAvoidingView  behavior="padding" style={styles.container}>
+      <SafeAreaView>
+        <OnboardingBackButton onPress={onBackButtonPressed}/>
          
         <Image 
-          style={{ 
-            width: 235, 
-            height: 65,
-            alignSelf: 'center',
-            marginBottom: 15,
-          }}
+          style={styles.stausBar}
           source={require("../assets/images/OnboardingStatusbar1.png")} />
         <Text
-          style={{
-            fontFamily: 'Nunito_700Bold',
-            fontSize: 36,
-            color: '#333',
-            marginHorizontal: 30,
-            textAlign: 'center',
-            marginBottom: 30,
-          }}>
+          style={styles.title}>
           Create your account!
         </Text>
 
       <Text
-          style={{
-            fontFamily: 'Nunito_400Regular',
-            fontSize: 16,
-            color: '#333',
-            marginLeft: 29,
-            textAlign: 'left',
-        }}> 
+          style={styles.text}> 
           Username <Text style={{color:'#FF0000'}}>*</Text>
         </Text>
         <View style={{alignItems: 'center'}} >
-        <InputField/>
+        <InputField
+        value={username}
+        setValue={setUsername}
+        />
         </View>
 
         <Text
-          style={{
-            fontFamily: 'Nunito_400Regular',
-            fontSize: 16,
-            marginLeft: 29,
-            textAlign: 'left',
-            marginTop: 3,
-        }}> 
+          style={styles.text}> 
           Full Name <Text style={{color:'#FF0000'}}>*</Text>
         </Text>
         <View style={{alignItems: 'center'}} >
-        <InputField/>
+        <InputField value={name}
+        setValue={setName}/>
         </View>
 
         <Text
-          style={{
-            fontFamily: 'Nunito_400Regular',
-            fontSize: 16,
-            color: '#333',
-            marginLeft: 29,
-            textAlign: 'left',
-            marginTop: 3,
-        }}> 
+          style={styles.text}> 
           School Email <Text style={{color:'#FF0000'}}>*</Text>
         </Text>
         <View style={{alignItems: 'center'}} >
-        <InputField/>
+        <InputField value={email}
+        setValue={setEmail}/>
         </View>
 
         <Text
-          style={{
-            fontFamily: 'Nunito_400Regular',
-            fontSize: 16,
-            color: '#333',
-            marginLeft: 29,
-            textAlign: 'left',
-            marginTop: 3,
-        }}> 
+          style={styles.text}> 
           Password <Text style={{color:'#FF0000'}}>*</Text>
         </Text>
 
         <View style={{alignItems: 'center', marginBottom: 30,}} >
-        <InputField/>
+        <InputField value={password}
+        setValue={setPassword}
+        secureTextEntry/>
         </View>
 
         <View style ={{alignItems: 'center'}}>
-        <OnboardingButton label={'Sign Up'} onPress={()=>navigation.navigate('Verification')} />
+        <OnboardingButton label={'Sign Up'} onPress={handleRegister} />
         </View>
     </SafeAreaView>
+
+    </KeyboardAvoidingView>
+    
   );
 };
 
 export default RegisterScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    backgroundColor: "#F4F6F9",
+  },
+  stausBar:{ 
+    width: 235, 
+    height: 65,
+    alignSelf: 'center',
+    marginBottom: 15
+  },
+  title: {
+    fontFamily: 'Nunito_700Bold',
+    fontSize: 36,
+    color: '#333',
+    marginHorizontal: 30,
+    textAlign: 'center',
+    marginBottom: 30,
+  }, 
+  text: {
+    fontFamily: 'Nunito_400Regular',
+    fontSize: 16,
+    marginLeft: 29,
+    textAlign: 'left',
+    marginTop: 3,
+    color: '#333',
+}
+
+})
